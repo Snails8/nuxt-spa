c8acb92a406673ce6cb4544834ee53524c072215
S-tanishi <tanichi.u.n.20@gmail.com>
2021-08-08 11:30:19 +0900

    add: input コンポーネントの作成

---
+++ components/partials/form/Input.vue
@@ -0,0 +1,38 @@
+<template>
+  <input
+      :type="type"
+      :name="name"
+      :value="value"
+      :placeholder="placeholder"
+      @input="updateValue"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    type: { type: String, required: true},
+    name: { type: String, required: true},
+    value: { type: String, required: true},
+    placeholder: { type: String, required: false},
+  },
+
+  methods: {
+    updateValue(e) {
+      this.$emit("input", e.target.value)
+    }
+  },
+};
+</script>
+
+<!-- 呼び出し元 例 -->
+<!--<template>-->
+
+<!--  <MyInput-->
+<!--      v-model="sampleForm.text"-->
+<!--      placeholder="サンプル"-->
+<!--      name="sample-input"-->
+<!--      type="text"-->
+<!--  ></MyInput>-->
+
+<!--</template>-->
