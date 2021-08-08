4d54f4071c742ca15fcb937d40a9e1b87ea0e4ce
S-tanishi <tanichi.u.n.20@gmail.com>
2021-08-08 11:34:10 +0900

    add: radio コンポーネントの作成

---
+++ components/partials/form/Radio.vue
@@ -0,0 +1,38 @@
+<template>
+  <fieldset>
+    <template v-for="(option, index) in options">
+      <label :key="index">
+        <input
+            type="radio"
+            :name="name"
+            :value="option.value"
+            @change="updateValue"
+        />{{ option.label }}
+      </label>
+    </template>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  props: {
+    value:   { type: String, required: true },
+    options: { type: Array, required: true },
+    name:    { type: String, required: true },
+  },
+
+  methods: {
+    updateValue: function(e) {
+      this.$emit("input", e.target.value);
+    }
+  }
+};
+</script>
+
+<!-- 呼び出し元の例 -->
+<!--  変数optionsには[{label: "hoge", value: "1"}, {label: "fuga", value: "2"}]のようなオブジェクトの配列を設定-->
+<!--  <MyRadio-->
+<!--      v-model="sampleForm.radio"-->
+<!--      name="sample-radio"-->
+<!--      :options="options"-->
+<!--  ></MyRadio>-->
