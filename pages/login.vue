ca6429d12fd663e942249f8f2533baa01278463d
S-tanishi <tanichi.u.n.20@gmail.com>
2021-10-23 11:59:41 +0900

    add: Login Componets の作成

---
+++ pages/login.vue
@@ -0,0 +1,63 @@
+<template>
+<!--  <div class="animated fadeIn">-->
+    <b-card class="mt-3 mx-3 d-flex align-items-center justify-content-center">
+      <b-alert variant="danger" :show="isFailure" dismissible @dismissed="isFailure=false">
+        {{ errorMessage }}
+      </b-alert>
+      <b-card-body>
+        <b-form name="login" @submit.prevent="login()">
+          <h1 class="h3 text-center mb-3">Login</h1>
+          <b-form-group>
+            <b-input-group>
+              <b-input-group-prepend>
+                <b-input-group-text><i class="fa fa-envelope"></i></b-input-group-text>
+              </b-input-group-prepend>
+              <b-form-input type="email" placeholder="Email" v-model="form.email"></b-form-input>
+            </b-input-group>
+          </b-form-group>
+          <b-form-group>
+            <b-input-group>
+              <b-input-group-prepend>
+                <b-input-group-text><i class="fa fa-asterisk"></i></b-input-group-text>
+              </b-input-group-prepend>
+              <b-form-input type="password" placeholder="Password" v-model="form.password"></b-form-input>
+            </b-input-group>
+          </b-form-group>
+          <div class="text-center col-12">
+            <b-button class="px-4" type="submit" variant="primary">Login</b-button>
+          </div>
+        </b-form>
+      </b-card-body>
+    </b-card>
+<!--  </div>-->
+</template>
+<script>
+export default {
+  name: 'login',
+  layout: 'clean',
+
+  data () {
+    return {
+      form: {
+        email: '',
+        password: ''
+      },
+      isFailure: false,
+      errorMessage: ''
+    }
+  },
+
+  methods: {
+    async login () {
+      try {
+        await this.$auth.loginWith('local', { data: this.form })
+
+      } catch (e) {
+        console.log(e)
+        this.isFailure = true
+        this.errorMessage = 'EmailかPasswordが間違っています'
+      }
+    }
+  }
+}
+</script>
